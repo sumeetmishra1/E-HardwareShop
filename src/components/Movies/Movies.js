5df386ba7785665822d5b5e7388068b1c0248927
sumeetmishra1 <sumitsfs0@gmail.com>
2024-03-13 23:57:09 +0530

    fetch-on-page-load

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,21 +1,35 @@
 import { Typography } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Movie from "./Movie";
 export  default function Movies (){
     const[movies,channgeMovie]=useState([])
     const[isLoading,changeLoadingState] = useState(false);
+    const[isError,setIsError] = useState(false)
+
+    useEffect(()=>{
+        movieHandler()
+    },[]);
     async function movieHandler(){
+        try{
         changeLoadingState(true)
+        setIsError(false)
         let data = await fetch('https://reqres.in/api/users?page=2')
         data = await data.json()
         changeLoadingState(false)
-       channgeMovie(data.data)
+        channgeMovie(data.data)
+        }
+        catch(e){
+            changeLoadingState(false)
+            setIsError(true)
+        }
+        
     }
     return(
 
         <div style={{display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
         <Typography sx={{mt:10,color:'black',fontFamily:'serif',fontSize:'60px',textAlign:'center'}}>Users</Typography>
         <button onClick={movieHandler}>Fetch Users</button>
+        {isError && <p>Something Went Wrong</p>}
         {isLoading?<h1>Loading</h1>:
         movies.map(movie=>{
           return <Movie details={movie}/>
